d47e649ea9437a7aa54e0a5e08385b13518d9cc9
Satvik Nagpal <satvik219@gmail.com>
2018-05-17 12:01:55 -0700

    I removed the supply subclasses because all of the work can be done in one class, supplies when the board is first instantiated.

--- Supplies.java
+++ Supplies.java
@@ -1,4 +1,4 @@
-
+import java.awt.geom.Point2D;
 /**
  * Write a description of class Supplies here.
  *
@@ -8,37 +8,75 @@
 public class Supplies
 {
     // instance variables - replace the example below with your own
-    private int myAmount;
+    private int myWater;
+    private int myWood;
+    private int myMetal;
+    private int myFood;
+    private int myFur;
+    private int waterCapacity;
+    private int woodCapacity;
+    private int metalCapacity;
+    private int foodCapacity;
+    private int furCapacity;
+    private Point2D.Double point;
 
     /**
      * Constructor for objects of class Supplies
      */
     public Supplies()
     {
-        myAmount = 0;
+        myWater = 0;
+        myWood = 0;
+        myMetal = 0;
+        myFood = 0;
+        myFur = 0;
+        waterCapacity = 0;
+        woodCapacity = 0;
+        metalCapacity = 0;
+        foodCapacity = 0;
+        furCapacity = 0;
+        point = new Point2D.Double(0.0, 0.0);
     }
     
     /**
      * Constructor that initializes myAmount to the amount specified
      * @param amount amount of supply initially
      */
-    public Supplies(int amount)
+    public Supplies(int water, int wood, int metal, int food, int fur, double x, double y)
     {
-        myAmount = amount;
+        myWater = water;
+        myWood = wood;
+        myMetal = metal;
+        myFood = food;
+        myFur = fur;
+        waterCapacity = water;
+        woodCapacity = wood;
+        metalCapacity = metal;
+        foodCapacity = food;
+        furCapacity = fur;
+        point = new Point2D.Double(x, y);
     }
 
-    public void add(int x)
+    public void empty()
     {
-        myAmount += x;
-    }
-
-    public void subtract(int x)
-    {
-        myAmount -= x;
+        myWater = 0;
+        myWood = 0;
+        myMetal = 0;
+        myFood = 0;
+        myFur = 0;
     }
     
-    public int getNum()
+    public void replenish()
     {
-        return myAmount;
+        myWater = waterCapacity;
+        myWood = woodCapacity;
+        myMetal = metalCapacity;
+        myFood = foodCapacity;
+        myFur = furCapacity;
+    }
+    
+    public Point2D.Double getPos()
+    {
+        return point;
     }
 }
