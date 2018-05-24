2c39a5789f9a99abba45818fc2f6ac44894a8107
Varun Agarwal <varunagarwal004@gmail.com>
2018-05-23 22:59:23 -0700

    GUI updates. Looks cleaner. Has working start, instructions, and credits button. Need to add instructions and credits.

--- GameRunner.java
+++ GameRunner.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.Screen;
 import javafx.event.ActionEvent;
@@ -19,6 +20,8 @@ import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 
@@ -34,7 +37,7 @@ import java.util.ArrayList;
 public class GameRunner extends Application 
 {
     // double to hold the speed of movement of the character
-    static int moveSpeed = 10;
+    static final int moveSpeed = 10;
     // will hold the size of the window
     static int windowX, windowY;
     // border around the edge where player cannot travel
@@ -55,8 +58,6 @@ public class GameRunner extends Application
         windowX = (int)((double)(bounds.getWidth() * 2) / 3);
         windowY = (int)((double)(bounds.getHeight() * 2) / 3);
 
-        Board b = new Board();
-
         // create a VBox to organize the other nodes
         VBox vb = new VBox(20);
 
@@ -65,27 +66,29 @@ public class GameRunner extends Application
         // Scene with the start menu
         Scene startScreen = new Scene(r1);
         // make a new borderpane to organize buttons
-        BorderPane bp = new BorderPane();
+        GridPane gp = new GridPane();
         
         // put the screen on the stage
         theStage.setScene(startScreen);
 
         // Button to start the game
-        Button startB = new Button("Start");
+        Button startB = new Button("   Start   ");
         // Button to start the instructions
-        Button instB = new Button("Instructions");
+        Button instB = new Button("How to play");
+        // Button to show credits
+        Button creditB = new Button("Credits");
+        // make a welcome text
+        Label welcome = new Label("Welcome to Survivor");
         
         // add the buttons to the VBox
-        vb.getChildren().addAll(startB, instB);
-
-        bp.setCenter(vb);
-        bp.setTop(new Label("Welcome to Survivor"));
+        vb.getChildren().addAll(welcome, startB, instB, creditB);
+        vb.setPrefWidth(windowX / 5);
+        vb.setPrefHeight(windowY / 3);
+        
+        vb.setAlignment(Pos.CENTER);
         
         // add the VBox to the scene
-        r1.getChildren().add(bp);
-
-        //r1.getChildren().add(startB);
-        //r1.getChildren().add(instB);
+        r1.getChildren().add(vb);
 
         
         // When the start button is pressed
@@ -93,189 +96,275 @@ public class GameRunner extends Application
             {
                 @Override public void handle(ActionEvent e)
                 {
-                    // Close the menu window
-                    theStage.close();
-                    // create a new window
-                    Stage game = new Stage();
-                    // set the window title
-                    game.setTitle("Survivor");
-                    // create the top of the nodes
-                    Group root = new Group();
-                    // create a new scene
-                    Scene theScene = new Scene(root);
-                    // set the scene to the window
-                    game.setScene(theScene);
-
-                    // create a canvas for the game
-                    Canvas c = new Canvas(windowX, windowY);
-                    // add canvas to group
-                    root.getChildren().add(c);
-
-                    // create a graphics context
-                    GraphicsContext gc = c.getGraphicsContext2D();
-
-                    // make two new images
-                    // person image
-                    Image pImage = new Image("Person.jpeg");
-                    // map image
-                    Image map = new Image("FakeMap.gif");
-
-                    // create a player
-                    Player p = new Player();
-                    // set size of player
-                    p.setWidthAndHeight(windowX /80.0, windowY / 30.0);
-
-                    // Rectangle with the size of the map in it
-                    Rectangle mapBox = new Rectangle((int)map.getWidth(), (int)map.getHeight());
-
-                    // holds the inputs
-                    ArrayList<String> input = new ArrayList<String>();
-
-                    // when a key is pressed
-                    theScene.setOnKeyPressed(
-                        new EventHandler<KeyEvent>()
-                        {
-                            public void handle(KeyEvent e)
-                            {
-                                // keycode
-                                String code = e.getCode().toString();
-
-                                // only add once... prevent duplicates
-                                if (!input.contains(code))
-                                    input.add(code);
-                            }
-                        }
-                    );
-
-                    // when the key is released
-                    theScene.setOnKeyReleased(
-                        new EventHandler<KeyEvent>()
-                        {
-                            public void handle(KeyEvent e)
-                            {
-                                // remove code from arraylist
-                                String code = e.getCode().toString();
-                                input.remove(code);
-                            }
-                        }
-                    );
-
-                    // make a new AnimationTimer 
-                    new AnimationTimer()
-                    {
-                        public void handle(long currentNanoTime)
-                        {
-                            // refresh the canvas
-                            gc.setFill(new Color(0, 0, 0, 0));
-                            gc.fillRect(0, 0, windowX, windowY);
-
-                            // if the keypressed is LEFT, RIGHT, DOWN, or UP
-                            // and the player will not move out of the board
-                            // move the player to the desired location
-                            if(input.contains("LEFT") && p.getX() >= moveSpeed + BORDER)
-                                p.move(p.getX() - moveSpeed, p.getY());
-                            else
-                            if(input.contains("RIGHT") && p.getX() <= windowX - p.getWidth() - moveSpeed - BORDER)
-                                p.move(p.getX() + moveSpeed, p.getY());
-                            else
-                            if(input.contains("DOWN") && p.getY() <= windowY - p.getHeight() - moveSpeed - BORDER)
-                                p.move(p.getX(), p.getY() + moveSpeed);
-                            else
-                            if(input.contains("UP") && p.getY() >= moveSpeed + BORDER)
-                                p.move(p.getX(), p.getY() - moveSpeed);
-
-                    
-                            // draw the map and the players again        
-                            gc.drawImage(map, 0, 0, windowX, windowY);
-                            gc.drawImage(pImage, p.getX(), p.getY(), p.getWidth(), p.getHeight());
-                        }
-                    }.start();
-
-                    // if(p.isAtObstacle())
-                    // {
-
-                        // Group g3 = new Group();
-                        // Scene pop = new Scene(g3);
-                        // Stage popup = new Stage();
-                        // VBox vb2 = new VBox();
-                        // HBox hb = new HBox();
-
-                        // Text obstacle = new Text(p.getObstacle().toString());
-                        // Button attackB = new Button("Attack");
-                        // Text damageT = new Text("" + p.getDamage());
-
-                        // hb.getChildren().addAll(attackB, damageT);
-
-                        // vb2.getChildren().add(hb);
-
-                        // g3.getChildren().add(vb2);
-                        // popup.showAndWait();
-                    // }
-
-                    // show the window with the game
-                    game.show();
-
+                    // start the game
+                    startGame(theStage);
                 }
             });
-
+  
         // when the instructions button is pressed
         instB.setOnAction(new EventHandler<ActionEvent>()
             {
                 @Override public void handle(ActionEvent e)
                 {
-                    // close the menu
-                    theStage.close();
-
-                    // create a new group of nodes
-                    Group instG = new Group();
-                    // make a new window
-                    Stage instStage = new Stage();
-
-                    // set the title of the window
-                    instStage.setTitle("Instructions");
-
-                    // create a VBox to organize the texts
-                    VBox textVB = new VBox();
-
-                    // button to go back to the main menu
-                    Button backB = new Button("Back");
-
-                    // create and add scene with instructions
-                    Scene instScene = new Scene(instG);
-                    instStage.setScene(instScene);
-
-                    // label the window
-                    Label title = new Label("Instructions:");
-                    // create two 'paragraphs'
-                    Text p1 = new Text(" 1 ");
-                    Text p2 = new Text(" 2 ");
-
-                    // add everything to VBox
-                    textVB.getChildren().addAll(title, p1, p2, backB);
-
-                    // add VBox to window
-                    instG.getChildren().add(textVB);
-
-                    // when the back button is pressed
-                    backB.setOnAction(new EventHandler<ActionEvent>()
-                        {
-                            @Override public void handle(ActionEvent e)
-                            {
-                                // close instructions window
-                                instStage.close();
-                                // show the main menu window
-                                theStage.show();
-                            }
-                        }
-                    );
-
-                    // show the instructions window
-                    instStage.show();
+                    // show the instructions page
+                    showInstructions(theStage);
+                }
+            });
+            
+        // When the credits button is pressed
+        creditB.setOnAction(new EventHandler<ActionEvent>()
+            {
+                @Override public void handle(ActionEvent e)
+                {
+                    // show credits
+                    showCredits(theStage);
                 }
             });
 
         // show the main menu window
         theStage.show();
+    }
+    
+    private static void startGame(Stage theStage)
+    {
+        // Close the menu window
+        theStage.close();
+        // create a new window
+        Stage game = new Stage();
+        // set the window title
+        game.setTitle("Survivor");
+        // create the top of the nodes
+        Group root = new Group();
+        // create a new scene
+        Scene theScene = new Scene(root);
+        // set the scene to the window
+        game.setScene(theScene);
+    
+        // create a canvas for the game
+        Canvas c = new Canvas(windowX, windowY);
+        // add canvas to group
+        root.getChildren().add(c);
+    
+        // create a graphics context
+        GraphicsContext gc = c.getGraphicsContext2D();
+    
+        // make two new images
+        // person image
+        Image pImage = new Image("Person.jpeg");
+        // map image
+        Image map = new Image("FakeMap.gif");
+    
+        // create a player
+        Player p = new Player();
+        // set size of player
+        p.setWidthAndHeight(windowX /80.0, windowY / 30.0);
+    
+        // Rectangle with the size of the map in it
+        Rectangle mapBox = new Rectangle((int)map.getWidth(), (int)map.getHeight());
+    
+        // holds the inputs
+        ArrayList<String> input = new ArrayList<String>();
+    
+        // when a key is pressed
+        theScene.setOnKeyPressed(
+            new EventHandler<KeyEvent>()
+            {
+                public void handle(KeyEvent e)
+                {
+                    // keycode
+                    String code = e.getCode().toString();
+    
+                    // only add once... prevent duplicates
+                    if (!input.contains(code))
+                        input.add(code);
+                }
+            }
+        );
+    
+        // when the key is released
+        theScene.setOnKeyReleased(
+            new EventHandler<KeyEvent>()
+            {
+                public void handle(KeyEvent e)
+                {
+                    // remove code from arraylist
+                    String code = e.getCode().toString();
+                    input.remove(code);
+                }
+            }
+        );
+    
+        // make a new AnimationTimer 
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                // refresh the canvas
+                gc.setFill(new Color(0, 0, 0, 0));
+                gc.fillRect(0, 0, windowX, windowY);
+    
+                // if the keypressed is LEFT, RIGHT, DOWN, or UP
+                // and the player will not move out of the board
+                // move the player to the desired location
+                if(input.contains("LEFT") && p.getX() >= moveSpeed + BORDER)
+                    p.move(p.getX() - moveSpeed, p.getY());
+                else
+                if(input.contains("RIGHT") && p.getX() <= windowX - p.getWidth() - moveSpeed - BORDER)
+                    p.move(p.getX() + moveSpeed, p.getY());
+                else
+                if(input.contains("DOWN") && p.getY() <= windowY - p.getHeight() - moveSpeed - BORDER)
+                    p.move(p.getX(), p.getY() + moveSpeed);
+                else
+                if(input.contains("UP") && p.getY() >= moveSpeed + BORDER)
+                    p.move(p.getX(), p.getY() - moveSpeed);
+    
+        
+                // draw the map and the players again        
+                gc.drawImage(map, 0, 0, windowX, windowY);
+                gc.drawImage(pImage, p.getX(), p.getY(), p.getWidth(), p.getHeight());
+            }
+        }.start();
+    
+        // if(p.isAtObstacle())
+        // {
+    
+            // Group g3 = new Group();
+            // Scene pop = new Scene(g3);
+            // Stage popup = new Stage();
+            // VBox vb2 = new VBox();
+            // HBox hb = new HBox();
+    
+            // Text obstacle = new Text(p.getObstacle().toString());
+            // Button attackB = new Button("Attack");
+            // Text damageT = new Text("" + p.getDamage());
+    
+            // hb.getChildren().addAll(attackB, damageT);
+    
+            // vb2.getChildren().add(hb);
+    
+            // g3.getChildren().add(vb2);
+            // popup.showAndWait();
+        // }
+    
+        // show the window with the game
+        game.show();
+
+    }
+    
+    private static void showInstructions(Stage theStage)
+    {
+        // close the menu
+        theStage.close();
+
+        // create a new group of nodes
+        Group instG = new Group();
+        // make a new window
+        Stage instStage = new Stage();
+
+        // set the title of the window
+        instStage.setTitle("Instructions");
+
+        // create a VBox to organize the texts
+        VBox textVB = new VBox(20);
+
+        // button to go back to the main menu
+        Button backB = new Button("Back");
+
+        // create and add scene with instructions
+        Scene instScene = new Scene(instG);
+        instStage.setScene(instScene);
+
+        // label the window
+        Label title = new Label("Instructions:");
+        // create two 'paragraphs'
+        Text p1 = new Text(" 1 ");
+        Text p2 = new Text(" 2 ");
+
+        // add everything to VBox
+        textVB.getChildren().addAll(title, p1, p2, backB);
+        textVB.setPrefWidth(windowX / 3);
+        textVB.setPrefHeight(windowY / 3);
+        
+        textVB.setAlignment(Pos.CENTER_LEFT);
+        
+        
+        // add VBox to window
+        instG.getChildren().add(textVB);
+
+        // when the back button is pressed
+        backB.setOnAction(new EventHandler<ActionEvent>()
+            {
+                @Override public void handle(ActionEvent e)
+                {
+                    // close instructions window
+                    instStage.close();
+                    // show the main menu window
+                    theStage.show();
+                }
+            }
+        );
+
+        // show the instructions window
+        instStage.show();
+    }
+    
+    private static void showCredits(Stage theStage)
+    {
+        theStage.close();
+        
+        // create a new group of nodes
+        Group credG = new Group();
+        // make a new window
+        Stage credStage = new Stage();
+
+        // set the title of the window
+        credStage.setTitle("Credits");
+
+        // create a VBox to organize the texts
+        VBox textVB = new VBox(20);
+
+        // button to go back to the main menu
+        Button backB = new Button("Back");
+
+        // create and add scene with instructions
+        Scene credScene = new Scene(credG);
+        credStage.setScene(credScene);
+
+        // label the window
+        Label title = new Label("Credits");
+        // create two 'paragraphs'
+        Text p1 = new Text(" 1 ");
+        Text p2 = new Text(" 2 ");
+        Text p3 = new Text(" 3 ");
+        Text p4 = new Text(" 4 ");
+        Text p5 = new Text(" 5 ");
+        
+        // add everything to VBox
+        textVB.getChildren().addAll(title, p1, p2, p3, p4, p5, backB);
+        textVB.setPrefWidth(windowX / 3);
+        textVB.setPrefHeight(windowY / 3);
+        
+        textVB.setAlignment(Pos.CENTER_LEFT);
+        
+        
+        // add VBox to window
+        credG.getChildren().add(textVB);
+
+        // when the back button is pressed
+        backB.setOnAction(new EventHandler<ActionEvent>()
+            {
+                @Override public void handle(ActionEvent e)
+                {
+                    // close instructions window
+                    credStage.close();
+                    // show the main menu window
+                    theStage.show();
+                }
+            }
+        );
+
+        // show the instructions window
+        credStage.show();
     }
 }
 
