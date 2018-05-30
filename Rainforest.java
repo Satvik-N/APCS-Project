3b794f1da1c76d1b9b83cfe88db41b17a29998cd
Satvik Nagpal <satvik219@gmail.com>
2018-05-30 12:02:14 -0700

    Edited Obstacle and Supply layout in Rainforest and Desert

--- Rainforest.java
+++ Rainforest.java
@@ -13,5 +13,38 @@ public class Rainforest extends Biomes
     }
     public void buildObstacles()
     {
+        for (int row = 0; row < biome.length * 0.2; row++)
+        {
+            for (int col = 0; col < biome[row].length; col++)
+            {
+                if (Math.random() < (1.0/10.0))
+                {
+                    if (Math.random() < 0.5)
+                        biome[row][col] = new Drought(col, row);
+                    else
+                        biome[row][col] = new Sandstorm(col, row);
+                }
+            }
+        }
+        for (int row = 24; row < biome.length * 0.4 + 24; row++)
+        {
+            for (int col = 0; col < biome[row].length; col++)
+            {
+                if (Math.random() < (1.0/10.0))
+                    biome[row][col] = new Animal(col, row);
+            }
+        }
+        for (int row = 72; row < biome.length * 0.4 + 72; row++)
+        {
+            for (int col = 0; col < biome[row].length; col++)
+            {
+                if (Math.random() < (1.0/8.0))
+                    biome[row][col] = new Mudslide(col, row);
+            }
+        }
+    }
+    public void buildSupplies()
+    {
+        
     }
 }
