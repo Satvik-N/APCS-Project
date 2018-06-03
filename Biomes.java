d946e259ab99ad644a89c2e22b2c19a50bec46b4
Satvik Nagpal <satvik219@gmail.com>
2018-06-03 13:38:44 -0700

    Java docced the Biome class

--- Biomes.java
+++ Biomes.java
@@ -7,14 +7,26 @@ import java.awt.geom.*;
  */
 public abstract class Biomes
 {
-    protected Materials[][] biome;
+    protected Materials[][] biome; //grid of materials
     public abstract void buildObstacles();
     
+    /**
+     *  <b>Summary</b> Method isObstacle() - determines whether there is an obstacle at the given location
+     *  @param x x coordinate of location
+     *  @param y y coordinate of location
+     *  @return boolean that tells if there is an obstacle at a location
+     */
     public boolean isObstacle (int x, int y)
     {
         return biome[y][x] instanceof Obstacles;
     }
     
+    /**
+     *  <b>Summary</b> Method getMaterial() - returns material at a certain location
+     *  @param x x coordinate of location
+     *  @param y y coordinate of location
+     *  @return Material that is at the location
+     */
     public Materials getMaterial(int x, int y)
     {
         return biome[y][x];
