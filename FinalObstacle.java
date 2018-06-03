a14756f9fc97c5b8c7f1ee8e8ee9d65338f82e5e
Priya K <pkactive12@gmail.com>
2018-06-02 18:46:33 -0700

    made final obstacle method

---
+++ FinalObstacle.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class FinalObstacle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FinalObstacle extends Obstacles
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    // health, water, and food 
+
+    /**
+     * Constructor for objects of class FinalObstacle
+     */
+    public FinalObstacle()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
